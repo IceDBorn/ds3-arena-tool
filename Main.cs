d999d72fe697e785f0e3f11604264a67de53e14b
IceDBorn <icedborn@gmail.com>
2020-03-28 22:33:21 +0200

    Add Sounds Change code formatting

--- Main.cs
+++ Main.cs
@@ -1,122 +1,67 @@
 ﻿using System;
 using System.IO;
-using System.Net;
-using System.Runtime.InteropServices;
-using System.Threading;
 using System.Windows.Forms;
-using System.Windows.Input;
 
 namespace DS3_Arena_Tool {
     public partial class Main : Form {
         private readonly GlobalKeyboardHook _backup = new GlobalKeyboardHook();
         private readonly GlobalKeyboardHook _restore = new GlobalKeyboardHook();
+        
         public Main() {
             InitializeComponent();
             winsLabel.Text = File.ReadAllText("wins.txt");
         }
         
-        private void button1_Click(object sender, EventArgs e) {
-            var flag = false;
-            try {
-                var sourceDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
-                                      "//DarkSoulsIII";
-                const string targetDirectory = @"Backup";
-                Replacer.Copy(sourceDirectory, targetDirectory);
-            }
-            catch
-            {
-                const bool flag2 = false;
-                var error = new Error(flag2);
-                error.ShowDialog();
-                flag = true;
-            }
-
-            try {
-                var wins = File.ReadAllText("wins.txt");
-                var winsInt = int.Parse(wins);
-                winsInt++;
-                wins = winsInt.ToString();
-                if (flag.Equals(false)) {
-                    System.IO.File.WriteAllText("wins.txt", wins);
-                }
-            }
-            catch {
-                System.IO.File.WriteAllText("wins.txt", "0");
-            }
-            winsLabel.Text = File.ReadAllText("wins.txt");
-            button3.Focus();
-        }
-
-        private void button2_Click(object sender, EventArgs e) {
-            try {
-                var targetDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
-                                      "//DarkSoulsIII";
-                const string sourceDirectory = @"Backup";
-                Replacer.Copy(sourceDirectory, targetDirectory);
-            }
-            catch {
-                const bool flag2 = true;
-                var error = new Error(flag2);
-                error.ShowDialog();
-            }
-            button3.Focus();
-        }
-
-        private void Main_Resize(object sender, EventArgs e) {
-            if (FormWindowState.Minimized != WindowState) return;
-            Hide();
-            this.notifyIcon1.Visible = true;
-        }
-
-        private void notifyIcon1_MouseMove(object sender, MouseEventArgs e) {
-            notifyIcon1.Text = "Current Wins: " + File.ReadAllText("wins.txt");
-        }
-        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e) {
-            Show();
-            WindowState = FormWindowState.Normal;
-            this.notifyIcon1.Visible = false;
-        }
-
         private void Main_Load(object sender, EventArgs e) {
             _backup.HookedKeys.Add(Keys.F9);
             _restore.HookedKeys.Add(Keys.F10);
             _backup.KeyDown += new KeyEventHandler(backup_KeyDown);
             _restore.KeyDown += new KeyEventHandler(restore_KeyDown);
         }
-
-        private void backup_KeyDown(object sender, KeyEventArgs e) {
-            var sourceDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
-                                  + "//DarkSoulsIII"; 
-            const string targetDirectory = @"Backup";
-            try {
-                var wins = File.ReadAllText("wins.txt");
-                var winsInt = int.Parse(wins);
-                winsInt++;
-                wins = winsInt.ToString();
-                System.IO.File.WriteAllText("wins.txt", wins);
-            }
-            catch {
-                System.IO.File.WriteAllText("wins.txt", "0");
-            }
-
-            Replacer.Copy(sourceDirectory, targetDirectory);
+        
+        private void Main_Resize(object sender, EventArgs e) {
+            if (FormWindowState.Minimized != WindowState) return;
+            Hide();
+            this.notifyIcon1.Visible = true;
+        }
+        
+        private void button1_Click(object sender, EventArgs e) {
+            Replacer.Backup(1);
             winsLabel.Text = File.ReadAllText("wins.txt");
-            e.Handled = true ;
+            button3.Focus();
         }
 
-        private static void restore_KeyDown(object sender, KeyEventArgs e) {
-            var targetDirectory =
-                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "//DarkSoulsIII";
-            const string sourceDirectory = @"Backup";
-            Replacer.Copy(sourceDirectory, targetDirectory);
-            e.Handled = true;
+        private void button2_Click(object sender, EventArgs e) {
+            Replacer.Restore(1);
+            button3.Focus();
         }
-
+        
         private void button4_Click(object sender, EventArgs e) {
             var editor = new EditWins();
             editor.ShowDialog();
             winsLabel.Text = File.ReadAllText("wins.txt");
             button3.Focus();
+        }
+
+        private void notifyIcon1_MouseMove(object sender, MouseEventArgs e) {
+            notifyIcon1.Text = "Current Wins: " + File.ReadAllText("wins.txt");
+        }
+        
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e) {
+            Show();
+            WindowState = FormWindowState.Normal;
+            this.notifyIcon1.Visible = false;
+        }
+        
+        private void backup_KeyDown(object sender, KeyEventArgs e) {
+            Replacer.Backup(0);
+            winsLabel.Text = File.ReadAllText("wins.txt");
+            e.Handled = true;
+        }
+
+        private static void restore_KeyDown(object sender, KeyEventArgs e) {
+            Replacer.Restore(0);
+            e.Handled = true;
         }
     }
 }
--- Replacer.cs
+++ Replacer.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.IO;
+using System.Media;
 
 namespace DS3_Arena_Tool {
     public static class Replacer {
-        public static void Copy(string sourceDirectory, string targetDirectory) {
+        private static void Copy(string sourceDirectory, string targetDirectory) {
             var diSource = new DirectoryInfo(sourceDirectory);
             var diTarget = new DirectoryInfo(targetDirectory);
  
@@ -22,6 +23,61 @@ namespace DS3_Arena_Tool {
                 var nextTargetSubDir =
                     target.CreateSubdirectory(diSourceSubDir.Name);
                 CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+
+        public static void Backup(int errorFlag) {
+            var flag = true;
+            try {
+                var sourceDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
+                                      "//DarkSoulsIII";
+                const string targetDirectory = @"Backup";
+                Replacer.Copy(sourceDirectory, targetDirectory);
+                if (errorFlag.Equals(0)) {
+                    SystemSounds.Asterisk.Play();
+                }
+            }
+            catch {
+                flag = false;
+                if (errorFlag.Equals(1)) {
+                    var error = new Error(false);
+                    SystemSounds.Hand.Play();
+                    error.ShowDialog();
+                }
+            }
+
+            try {
+                var wins = File.ReadAllText("wins.txt");
+                var winsInt = int.Parse(wins);
+                winsInt++;
+                wins = winsInt.ToString();
+                if (flag.Equals(true)) {
+                    System.IO.File.WriteAllText("wins.txt", wins);
+                }
+            }
+            catch {
+                System.IO.File.WriteAllText("wins.txt", "0");
+            }
+        }
+
+        public static void Restore(int errorFlag)
+        {
+            try {
+                var targetDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
+                                      "//DarkSoulsIII";
+                const string sourceDirectory = @"Backup";
+                Replacer.Copy(sourceDirectory, targetDirectory);
+                if (errorFlag.Equals(0)) {
+                    SystemSounds.Asterisk.Play();
+                }
+            }
+            catch {
+                if (errorFlag.Equals(1)) {
+                    const bool flag = true;
+                    var error = new Error(flag);
+                    SystemSounds.Hand.Play();
+                    error.ShowDialog();
+                }
             }
         }
     }
